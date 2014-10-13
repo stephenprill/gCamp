02b488adba8ca60915b055366ccd5e71dec01a77
Stephen Prill <Prill@Stephens-MacBook-Pro.local>
2014-10-13 15:59:58 -0600

    About Us & Terms

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
   root "pages#index"
-  
+  get 'about' => 'pages#about_us', as: :about_us
+  get 'terms' => 'pages#terms', as: :terms
+
+  end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -56,4 +61,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
