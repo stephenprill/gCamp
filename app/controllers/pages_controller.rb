757743020a87518bd0e323375b8b0b51347dac3b
Stephen Prill <stephen.prill@gmail.com>
2014-10-15 14:14:11 -0600

    recfactoring arrays

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,21 @@
 class PagesController < ApplicationController
   def index
-    @quotes = ["foobar", "baz", "w00t"]
+
+
+    quote1 = Quote.new
+    quote1.text = "Big Gulps huh? Welp, see you later"
+    quote1.author = "Jim Carey"
+
+    quote2 = Quote.new
+    quote2.text = "Winter is coming."
+    quote2.author = "Ned Stark"
+
+    quote3 = Quote.new
+    quote3.text = "Turn down for what?"
+    quote3.author = "Lil Jon"
+
+    @quotes = [quote1, quote2, quote3]
+
+
   end
 end
---
+++ app/models/quote.rb
@@ -0,0 +1,7 @@
+class Quote
+
+  attr_accessor :author, :text
+
+
+
+end
