b0f0c04eafbd7fcc8d166bf56a00e6f1a9118708
Stephen Prill <stephen.prill@gmail.com>
2014-10-17 15:20:55 -0600

    0060-task-with-scaffold try 8

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   get 'about' => 'pages#about_us', as: :about_us
   get 'terms' => 'pages#terms', as: :terms
   get 'faq' => 'pages#faq', as: :faq
-  get 'tasks' => 'pages#tasks', as: :tasks
 
   end
 
