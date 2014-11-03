7768e9e7c4ce3e29dbb360578e02a120596fe7c8
Stephen Prill <stephen.prill@gmail.com>
2014-11-02 23:05:38 -0700

    Post Projects CRUD, Bootstrap Projects Begin

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,41 @@
 class ProjectsController < ApplicationController
 
   def index
-    @projects = Project.all 
+    @projects = Project.all
 
+  end
+
+  def new
+    @projects = Project.new
+  end
+
+  def create
+    @projects = Project.new
+    project_params = params.require(:project).permit(:name)
+    @project = Project.new(project_params)
+    @project.save
+    redirect_to projects_path
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    project_params = params.require(:project).permit(:name)
+    @project = @project = Project.find(params[:id])
+    @project.update(project_params)
+    redirect_to project_path(@project)
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path(@project)
   end
 
 
