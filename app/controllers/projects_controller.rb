02f222fae3ba4c16bb58cf7ea57d7529cd1199b9
Stephen Prill <stephen.prill@gmail.com>
2014-11-02 23:43:53 -0700

    Attempting To Bootstrap Projects CRUD, added link in app for Projects to delete

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -13,9 +13,11 @@ class ProjectsController < ApplicationController
     @projects = Project.new
     project_params = params.require(:project).permit(:name)
     @project = Project.new(project_params)
-    @project.save
+    if @project.save
+      flash[:success] = "Project was successfully created"
     redirect_to projects_path
   end
+    end
 
   def show
     @project = Project.find(params[:id])
@@ -28,9 +30,11 @@ class ProjectsController < ApplicationController
   def update
     project_params = params.require(:project).permit(:name)
     @project = @project = Project.find(params[:id])
-    @project.update(project_params)
+    if @project.update(project_params)
+      flash[:success] = "Project was successfully updated"
     redirect_to project_path(@project)
   end
+    end
 
   def destroy
     @project = Project.find(params[:id])
