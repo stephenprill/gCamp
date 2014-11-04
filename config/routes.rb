561b87783d07b3cb669f528ce4f72314cc050474
Stephen Prill <stephen.prill@gmail.com>
2014-11-04 14:38:32 -0700

    Before adding rspec & Capybara

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,12 @@ Rails.application.routes.draw do
   get 'terms' => 'pages#terms', as: :terms
   get 'faq' => 'pages#faq', as: :faq
 
+  get '/sign-up' => 'registrations#new', as: :signup
+  post '/sign-up' => 'registrations#create'
+  get '/sign-in' => 'authentication#new', as: :signin
+  post '/sign-in' => 'authentication#create'
+  get '/sign-out' => 'authentication#destroy', as: :signout
+
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141103051049) do
+ActiveRecord::Schema.define(version: 20141103211754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20141103051049) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
+    t.string "password_digest"
   end
 
 end
