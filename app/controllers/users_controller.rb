d2aca98fc9c1578e21821f9ac43e1fedd5abfa60
Stephen Prill <stephen.prill@gmail.com>
2014-11-26 17:00:37 -0700

    changed user controller to redirect to edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
     else
       render :new
     end
-    end
+  end
 
   def edit
     @user = User.find(params[:id])
@@ -28,8 +28,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(users_param)
       flash[:success] = "User was successfully updated"
-    redirect_to users_path
-  end
+      redirect_to users_path
+    else
+      render :edit
+    end
   end
 
   def show
@@ -41,8 +43,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.destroy
       flash[:success] = "User was successfully deleted"
-    redirect_to users_path
-  end
+      redirect_to users_path
+    end
   end
 
 
