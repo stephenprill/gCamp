102381a1a80f4340f80da3e50dcc88951f52a769
Stephen Prill <stephen.prill@gmail.com>
2014-11-28 12:29:27 -0700

    adjusted projects index before membershipmvp submission

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -15,11 +15,11 @@ class MembershipsController < ApplicationController
 
 
   def create
-    # p @project.membership.length saved 
+    # p @project.membership.length saved
     @membership = @project.memberships.new(membership_params)
     #@membership = Membership.new(params.require(:membersihp).permit(:user_id, :role))
     if @membership.save
-      redirect_to project_memberships_path(@project), notice: "#{@membership.user.full_name} was succesfully created"
+      redirect_to project_memberships_path(@project), notice: "#{@membership.user.full_name} was added successfully"
     else
       # p @project.membership.length not saved in dB then hands both to view and tries to iterate over them
       @project_memberships = @project.memberships - [@membership]
@@ -28,19 +28,17 @@ class MembershipsController < ApplicationController
   end
 
 
-
-
-
   def update
     @membership = Membership.find(params[:id])
     @membership.update(membership_params)
-    redirect_to project_memberships_path(@project), notice: "#{@membership.user.full_name} was succesfully updated"
+    redirect_to project_memberships_path(@project), notice: "#{@membership.user.full_name} was updated successfully"
   end
+
 
   def destroy
     @membership = Membership.find(params[:id])
     @membership.destroy
-    redirect_to project_path(@project), notice: "#{@membership.user.full_name} was succesfully deleted"
+    redirect_to project_path(@project), notice: "#{@membership.user.full_name} was removed successfully"
   end
 
 
