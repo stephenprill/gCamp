23b27ba180aac00e37b238625b3f40c187241785
Stephen Prill <stephen.prill@gmail.com>
2014-11-30 16:02:19 -0700

    adjusting about page for heroku push

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,24 @@
 class CommentsController < ApplicationController
 
-  before_action do
-    @project = Project.find(params[:project_id])
-    @task = @project.tasks.find(params[:task_id])
-  end
+    before_action do
+      @project = Project.find(params[:project_id])
+      @task = @project.tasks.find(params[:task_id])
+    end
 
 
-  def create
-    @comment = @task.comments.new(params.require(:comment).permit(:comment, :user_id, :task_id))
-    @comment.user_id = current_user.id
-    #@comment =
-    # @comment.save
+    def create
+      @comment = @task.comments.new(params.require(:comment).permit(:comment, :user_id, :task_id))
+      @comment.user_id = current_user.id
 
-  if @comment.save
-    redirect_to project_task_path(@project, @task), notice: "Comment was successfully created."
-  else
-    render 'tasks/show'
-  end
-
-end
+      if @comment.save
+        redirect_to project_task_path(@project, @task), notice: "Comment was successfully created."
+      else
+        render 'tasks/show'
+      end
+    end
 
 end
+
+
+#@comment =
+# @comment.save
