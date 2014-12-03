5e5e42bf7c442cec2fd1de50d22072d62fd522d8
Stephen Prill <stephen.prill@gmail.com>
2014-12-02 21:52:41 -0700

    updated spec/features/projects_spec, all tests now passing

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -10,16 +10,16 @@ require 'rails_helper'
       fill_in "Name", with: "New App"
 
       click_on "Create Project"
-      expect(page).to have_content("Project was successfully created")
+      expect(page).to have_content("New App was successfully created")
 
       click_on "New App"
       click_on "Edit"
 
       fill_in "Name", with: "New App 2014"
       click_on "Update Project"
-      expect(page).to have_content("Project was successfully updated")
+      expect(page).to have_content("New App 2014 was successfully updated")
 
-      click_on "Destroy"
-      expect(page).to have_content("Project was successfully destroyed")
+      click_on "Delete"
+      expect(page).to have_content("New App 2014 was successfully destroyed")
     end
 end
