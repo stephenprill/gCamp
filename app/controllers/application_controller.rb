e130fe9cae15a1a3bb38c6996d78e89cd93d5de6
Stephen Prill <stephen.prill@gmail.com>
2014-12-04 16:44:55 -0700

    adjusted private controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,10 +11,11 @@ class ApplicationController < ActionController::Base
   end
 end
 
-def current_user
-  #session[:user_id]
-  User.find_by(id: session[:user_id])
-end
+  def current_user
+    if session[:user_id]
+    User.find_by(id: session[:user_id])
+    end
+  end
 
 
   helper_method :current_user
--- app/controllers/private_controller.rb
+++ app/controllers/private_controller.rb
@@ -2,11 +2,12 @@ class PrivateController < ApplicationController
   # views will look for layouts/public then layouts/application
   protect_from_forgery with: :exception
 
-#   layout :determine_layout
-#
-#   private
-#
-#   def determine_layout
-#     current_user ? "private" : "public"
-#   end
-# end
+  #   layout :determine_layout
+  #
+  #   private
+  #
+  #   def determine_layout
+  #     current_user ? "private" : "public"
+  #   end
+  
+end
