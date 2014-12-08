bf24294da01c311968037f72a629d88942e38f35
Stephen Prill <stephen.prill@gmail.com>
2014-12-07 20:01:34 -0700

    projects_spec passing

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -3,23 +3,37 @@ require 'rails_helper'
 
   feature "Projects" do
 
-    scenario 'User creates, edits and deletes project' do
+    scenario 'User logs in, creates, edits and deletes a project' do
+
       visit root_path
+      click_on "Sign Up"
+      fill_in("First name", {with: "Jameson"})
+      fill_in "Last name", with: "Jones"
+      fill_in "Email", with: "jameson@mail.com"
+      fill_in "Password", with: "pass"
+      fill_in "Password confirmation", with: "pass"
+      click_on "Sign up"
+      visit root_path
+      expect(page).to have_content("Jameson Jones")
+
+      visit root_path
+    
       click_on "Projects"
       click_on "Create Project"
+
       fill_in "Name", with: "New App"
 
       click_on "Create Project"
-      expect(page).to have_content("New App was successfully created")
+      expect(page).to have_content("New App was successfully created.")
 
       click_on "New App"
       click_on "Edit"
 
       fill_in "Name", with: "New App 2014"
-      click_on "Update Project"
-      expect(page).to have_content("New App 2014 was successfully updated")
+      click_on "Create Project"
+      expect(page).to have_content("New App 2014 was successfully updated.")
 
       click_on "Delete"
-      expect(page).to have_content("New App 2014 was successfully destroyed")
+      expect(page).to have_content("New App 2014 was successfully destroyed.")
     end
 end
