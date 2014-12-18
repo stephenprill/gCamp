b1c5f4fcea95d646148eb63dcff09a4670157aee
Stephen Prill <stephen.prill@gmail.com>
2014-12-17 20:05:05 -0700

    completed pivotal tracker integration for projects

--- app/controllers/tracker_projects_controller.rb
+++ app/controllers/tracker_projects_controller.rb
@@ -2,7 +2,8 @@ class TrackerProjectsController < ApplicationController
 
   def show
     tracker_projects = TrackerApi.new
-    @tracker_projects = tracker_projects.stories(current_user.pivotal_tracker_token, params[:id])
+    @tracker_stories = tracker_projects.stories(current_user.pivotal_tracker_token, params[:id])
+    # @project = Project.find(params[:id])
   end
 
 end
