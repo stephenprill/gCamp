88d300307b3eab61a44b83b5c957363e5947e2e8
Stephen Prill <stephen.prill@gmail.com>
2014-12-17 17:23:52 -0700

    added application helper for projects

--- app/models/tracker_api.rb
+++ app/models/tracker_api.rb
@@ -14,7 +14,6 @@ class TrackerApi
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  #added for stories check the API
   def stories(token, project_id)
     return [] if token.nil?
     response = @conn.get do |req|
